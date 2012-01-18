1294862ba43b067f39b966fd25c564f9980a938b
Elthariel <elthariel@gmail.com>
2012-01-18 11:57:09 +0100

    Implemented abilities, a starting point for oauth user information access

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_filter :login_or_oauth_required
+  authorize_resource
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    params[:id] = current_user.id if current_user and params[:id] == 'me'
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, :notice => "Successfully created user."
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :notice  => "Successfully updated user."
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url, :notice => "Successfully destroyed user."
+  end
+end
